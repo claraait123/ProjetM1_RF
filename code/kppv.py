0cda9e4b584ea0f87abf41d24ea1fd11b3275e2e
Clara AM <rabiaautop@gmail.com>
2025-12-09 21:10:55 +0100

    ajout vote majoritaire et méthode de calcul de distance

--- code/kppv.py
+++ code/kppv.py
@@ -9,10 +9,14 @@ def distance_manhattan(x, y):
     diff = x - y
     return np.sum(np.abs(diff))
 
+def distance_minkowski(x, y, p=2):
+    diff = np.abs(x - y)
+    return np.power(np.sum(diff ** p), 1.0 / p)
+
 def k_plus_proches_voisins_x(x, X_train, Y_train, k=3):
     distances = []
     for x_train_i in X_train:
-        distances.append(distance_manhattan(x, x_train_i))
+        distances.append(distance_minkowski(x, x_train_i))
     idx_knn = np.argsort(distances)[:k]
     labels_knn = Y_train[idx_knn]
 
